137dea2d24ec0cacf823fc6d5aaad0796fc5d859
provefar <provefars@gmail.com>
2020-04-12 23:31:02 +0800

    feat: add http status codes defines

---
+++ src/main/scala/example/HttpStatusCodes.scala
@@ -0,0 +1,18 @@
+package example
+
+abstract class HttpStatusCodes{
+  val statusCode: Int
+  val description: String
+}
+case class OK() extends HttpStatusCodes {
+  override val statusCode: Int = 200
+  override val description: String = "OK"
+}
+case class NotFound() extends HttpStatusCodes {
+  override val statusCode: Int = 404
+  override val description: String = "Page Not Found"
+}
+case class MethodNotAllowed() extends HttpStatusCodes {
+  override val statusCode: Int = 405
+  override val description: String = "Method Not Allowed"
+}
