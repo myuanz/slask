8122af034696bf010ec143a80ed23a61f848b490
provefar <provefars@gmail.com>
2020-04-16 11:15:44 +0800

    feat: 新增用户操作相关对象

---
+++ src/main/scala/example/Utils.scala
@@ -0,0 +1,28 @@
+package example
+
+import fun.myuan.slask.Utils.md5HashString
+
+object Utils
+
+case class User(email: String, password: String)
+
+case class EmailError(message: String) extends Exception(message)
+
+object Users {
+  var users: Map[String, User] = Map() // 相当于email是索引
+
+  def addUser(email: String, password: String): Unit = {
+    email match {
+      case i if !users.contains(i) => users = users ++ Map(email -> User(email, md5HashString(password)))
+      case _ => throw new Exception("Email already exists")
+    }
+  }
+
+  def checkPassword(email: String, password: String): Boolean = {
+    email match {
+      case i if users.contains(i) => users(email).password == md5HashString(password)
+      case _ => throw new Exception("Email does not exist")
+    }
+  }
+
+}
